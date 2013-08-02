da2818c8498b8bd2f072f56257842f4a19b61b88
ScottHaileRobertson <heyemailthis+Git@gmail.com>
2013-08-02 15:09:18 -0400

    Got itterative recon working!!! No penalization though... Also have decay correction working.
    Signed-off-by: ScottHaileRobertson <heyemailthis+Git@gmail.com>

--- ScottLibs/Distrib/Recon/MRI_Simulation/demo_itterative_recon.m
+++ ScottLibs/Distrib/Recon/MRI_Simulation/demo_itterative_recon.m
@@ -21,8 +21,11 @@ colorbar();
 axis image;
 
 % Create trajectories
-N = [Ndisp Ndisp]/4;
-[traj omega wi] = mri_trajectory('radial', {'na', 12}, N, fov, {'voronoi'});
+Nsz = Ndisp/4;
+N = [Nsz Nsz];
+nrays = 50;
+npts = Nsz;
+[traj omega wi] = mri_trajectory('radial', {'na', nrays, 'nr', npts-1}, N, fov, {'voronoi'});
 figure()
 plot(omega(:,1),omega(:,2),'.r');
 axis(pi*[-1 1 -1 1]);
@@ -36,9 +39,33 @@ Am = Gmri(traj, mask, 'fov', fov, 'nufft_args', {N,J,K,N/2,'minmax:kb'});
 
 % Calculate data
 data = obj.kspace(traj(:,1), traj(:,2));
+figure();
+surf(abs(reshape(data,[npts nrays])));
+colormap(jet);
+shading interp;
+title('Exact fft magnitude');
+
+% Decay signals randomly
+min_val = 0.3;
+flip_angle = acos(min_val^(1/(nrays-1)))
+decay_weight = cos(flip_angle).^([0:(nrays-1)]);
+decay_weight = repmat(decay_weight,[npts, 1]);
+decay_weight = decay_weight(:);
+data = data .* decay_weight;
+figure();
+surf(abs(reshape(data,[npts nrays])));
+colormap(jet);
+shading interp;
+title('Decayed fft magnitude');
 
 % Add noise
-data = data + 0.5 * randn(size(data));
+data = data + 0.05 * (max(abs(data(:)))) * (rand(size(data))-0.5);
+figure();
+surf(abs(reshape(data,[npts nrays])));
+colormap(jet);
+shading interp;
+title('Noisy fft magnitude');
+
 
 % Conjugate phase reconstruction
 recon_vol = Am' * (wi .* data) * prod(N/Ndisp);
@@ -50,28 +77,73 @@ axis image;
 title('Conjugate Phase Recon');
 colorbar();
 
-% Penalized Congugate Gradient with quadratic penalty
-niter = 100;
-beta = 100 % good for quadratic
-C = Cdiff(sqrt(beta) * mask, 'edge_type', 'tight');
-recon_pcgq = qpwls_pcg1(0*recon_vol(:), Am, 1, data, C, 'niter', niter)*prod(N/Ndisp);
-recon_pcgq  = embed(recon_pcgq (:,end), mask);
+% Congugate Gradient with no penalty
+niter = 20;
+weighting = Gdiag(decay_weight);
+recon_pcgq = qpwls_pcg1(0*recon_vol(:), Am, 1, data, 0, 'niter', niter, 'isave',1:niter)*prod(N/Ndisp);
+recon_pcgq  = reshape(recon_pcgq, [N niter]);
 figure();
-imagesc(abs(recon_pcgq));
+imslice(abs(recon_pcgq));
 colormap(gray);
 axis image;
-title('Penalized Conjugate gradient with quadratic penalty');
+title('Itterative reconstruction - no weighting');
 colorbar();
 
-% Penalized Congugate Gradient with edge-preserving penalty
-delta = 0.3;
-R = Robject(mask, 'edge_type', 'tight', 'type_denom', 'matlab', ...
-		'potential', 'hyper3', 'beta', beta, 'delta', delta);
-recon_pcg_edge = pwls_pcg1(0*recon_vol(:), Am, 1, data, R, 'niter', niter)/(4^2);
-recon_pcg_edge = embed(recon_pcg_edge, mask);
+% Congugate Gradient with no penalty
+niter = 20;
+weighting = Gdiag(decay_weight);
+recon_pcgq = qpwls_pcg1(0*recon_vol(:), Am, 1, data./decay_weight, 0, 'niter', niter, 'isave',1:niter)*prod(N/Ndisp);
+recon_pcgq  = reshape(recon_pcgq, [N niter]);
 figure();
-imagesc(abs(recon_pcgq));
+imslice(abs(recon_pcgq));
 colormap(gray);
 axis image;
-title('Penalized Conjugate gradient with edge-preserving penalty');
+title('Itterative reconstruction - naive weighting');
 colorbar();
+
+% Congugate Gradient with no penalty
+niter = 20;
+weighting = Gdiag(decay_weight);
+recon_pcgq = qpwls_pcg1(0*recon_vol(:), Am, weighting, data./decay_weight, 0, 'niter', niter, 'isave',1:niter)*prod(N/Ndisp);
+recon_pcgq  = reshape(recon_pcgq, [N niter]);
+figure();
+imslice(abs(recon_pcgq));
+colormap(gray);
+axis image;
+title('Itterative reconstruction - smart weighting');
+colorbar();
+
+% % Penalized Congugate Gradient with quadratic penalty
+% beta = 100 % good for quadratic
+% C = Cdiff(sqrt(beta) * mask, 'edge_type', 'tight');
+% recon_pcgq = qpwls_pcg1(0*recon_vol(:), Am, weighting, data, C, 'niter', niter, 'isave',1:niter)*prod(N/Ndisp);
+% recon_pcgq  = embed(recon_pcgq (:,end), mask);
+% figure();
+% imagesc(abs(recon_pcgq),[0 2]);
+% colormap(gray);
+% axis image;
+% title('Unweighted quadratic penalty');
+% colorbar();
+
+% % Penalized Congugate Gradient with edge-preserving penalty
+% delta = 0.3;
+% R = Robject(mask, 'edge_type', 'tight', 'type_denom', 'matlab', ...
+% 		'potential', 'hyper3', 'beta', beta, 'delta', delta);
+% recon_pcg_edge = pwls_pcg1(0*recon_vol(:), Am, weighting, data, R, 'niter', niter,'isave',1:niter)/(4^2);
+% recon_pcg_edge = embed(recon_pcg_edge, mask);
+% figure();
+% imagesc(abs(recon_pcgq));
+% colormap(gray);
+% axis image;
+% title('Unweighted edge-preserving penalty');
+% colorbar();
+% 
+% % Penalized Congugate Gradient with edge-preserving penalty
+% recon_pcg_edge = pwls_pcg1(0*recon_vol(:), Am, weighting, data, R, 'niter', niter,'isave',1:niter)/(4^2);
+% recon_pcg_edge = embed(recon_pcg_edge, mask);
+% figure();
+% imagesc(abs(recon_pcgq));
+% colormap(gray);
+% axis image;
+% title('Edge-preserving penalty with decay weights');
+% colorbar();
